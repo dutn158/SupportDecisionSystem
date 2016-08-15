6d22a81522e6f01b45783fcc4933495c52a3ff4f
Tran Du <untilyou158@gmail.com>
2016-08-15 09:14:19 +0700

    update

---
+++ src/com/dutn/Classification.java
@@ -0,0 +1,25 @@
+package com.dutn;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.dutn.dto.Model;
+
+public class Classification {
+
+	private final List<Model> models;
+	private HashMap<String, String> a;
+
+	public Classification(List<Model> models) {
+		this.models = models;
+	}
+
+	public void training() {
+
+	}
+
+	public void classify() {
+
+	}
+
+}
